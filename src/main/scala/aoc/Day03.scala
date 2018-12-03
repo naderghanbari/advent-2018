b5e38e8288adeafc12bd9e750c180ca928368c5a
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-12-03 01:44:20 -0500

    Day03 Second and First part

--- src/main/scala/aoc/Day03.scala
+++ src/main/scala/aoc/Day03.scala
@@ -1,35 +1,50 @@
 package aoc
 
+/** Using `par` is not exactly dumb here, it's applied on a
+  * cartesian product of two ranges so it provides a
+  * quasi-divide-and-conquer solution.
+  * This is way far from an optimum algorithm though.
+  */
 object Day03 extends App {
-  lazy val data = DataSource.linesFromTextFile("day-3-input.txt").toStream
-  lazy val rects = data.map(Claim.parse).map(_.toRect)
 
-  val chracteristics = for {
-    x <- (1 to 1000).view
-    y <- (1 to 1000).view
+  val data = DataSource.linesFromTextFile("day-3-input.txt")
+
+  val claims = data.map(Claim.parse).toSeq
+
+  val Some(winner) = claims.find(claim => claims.count(!_.disjoint(claim)) == 1)
+  println(s"Part B: ${winner.id}")
+
+  val rects = claims.map(_.rect)
+  val range = (1 to 1000).view.par
+  val hotSpotCount = for {
+    x <- range
+    y <- range
     p = Point(x, y)
-    c = if (rects.dropWhile(!_.covers(p)).drop(1).dropWhile(!_.covers(p)).nonEmpty) 1 else 0
-  } yield c
+    if rects.dropWhile(!_.covers(p)).drop(1).dropWhile(!_.covers(p)).nonEmpty
+  } yield 1
 
-  println(chracteristics.sum)
+  println(s"Part A: ${hotSpotCount.sum}")
+
 }
 
 case class Point(x: Int, y: Int)
+
 case class Rect(nw: Point, se: Point) {
   def covers(p: Point): Boolean = (p.x <= se.x) && (p.x >= nw.x) && (p.y <= se.y) && (p.y >= nw.y)
 }
-case class Claim(id: String, fromLeft: Int, fromTop: Int, width: Int, height: Int) {
-  def toRect = Rect(
-    Point(fromTop + 1, fromLeft + 1),
-    Point(fromTop + height, fromLeft + width)
-  )
+
+case class Claim(id: String, left: Int, top: Int, w: Int, h: Int) {
+  val rect = Rect(Point(top + 1, left + 1), Point(top + h, left + w))
+
+  def disjoint(that: Claim) =
+    (that.rect.nw.x > rect.se.x) || (that.rect.nw.y > rect.se.y) ||
+      (rect.nw.x > that.rect.se.x) || (rect.nw.y > that.rect.se.y)
 }
 
 object Claim {
-  private val ClaimPattern = "^#(\\d)+\\s+@\\s+(\\d+),(\\d+):\\s*(\\d+)x(\\d+)$".r
+  private val ClaimPattern = "^#(\\d+)\\s+@\\s+(\\d+),(\\d+):\\s*(\\d+)x(\\d+)$".r
 
   def parse(s: String): Claim = s match {
     case ClaimPattern(id, t, l, w, h) => Claim(id, t.toInt, l.toInt, w.toInt, h.toInt)
   }
 }
-
