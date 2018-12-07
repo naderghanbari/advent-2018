07fd4e2e7d972c34be575f8bff749771e14f13ce
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-12-06 23:58:52 -0500

    Day06 First and Second part (improvement)

--- src/main/scala/aoc/Day06.scala
+++ src/main/scala/aoc/Day06.scala
@@ -8,7 +8,6 @@ object Day06 extends App {
   val points = lines.map(_.split(",").map(_.trim.toInt).toList).map { case List(x, y) => Point(x, y) }
   val (xMin, xMax) = (points.map(_.x).min, points.map(_.x).max)
   val (yMin, yMax) = (points.map(_.y).min, points.map(_.y).max)
-  val radius = Seq(xMax - xMin, yMax - yMin).max
 
   def distTo(p: Point)(q: Point) = abs(p.x - q.x) + abs(p.y - q.y)
   def totalDistance(q: Point) = points.map(distTo(q)).sum
@@ -24,8 +23,8 @@ object Day06 extends App {
     c <- closest(Point(i, j))
   } yield c
 
-  val original = areaGrid(radius).groupBy(identity).mapValues(_.length)
-  val witness = areaGrid(radius + 5).groupBy(identity).mapValues(_.length)
+  val original = areaGrid(0).groupBy(identity).mapValues(_.length)
+  val witness = areaGrid(1).groupBy(identity).mapValues(_.length)
   val maxArea = original.collect { case (p, area) if witness.get(p).contains(area) => area }.max
 
   println(s"Part 1: $maxArea")
