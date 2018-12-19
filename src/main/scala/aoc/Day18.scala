38dbde1ce30693774d94efb3a821fc2f46a78578
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-12-18 23:14:24 -0500

    Day 18 part 2 (using streams)

--- src/main/scala/aoc/Day18.scala
+++ src/main/scala/aoc/Day18.scala
@@ -8,7 +8,6 @@ object Day18 extends App {
 
   val input = linesFromTextFile("day-18-input.txt").map(_.toVector.zipWithIndex).toVector.zipWithIndex
   val terrain = input.flatMap { case (rows, y) => rows.map { case (content, x) => Acre(y, x) -> content } }.toMap
-
   val initial = terrain
   val around = (-1 to 1).flatMap(dy => (-1 to 1).map(dy -> _)).toSet - (0 -> 0)
 
@@ -18,25 +17,32 @@ object Day18 extends App {
   }
 
   def change(state: State)(acre: Acre, contents: Char) = contents match {
-    case '.' if acre.adj(state)('|') >= 3                              => '|'
-    case '.'                                                           => '.'
-    case '|' if acre.adj(state)('#') >= 3                              => '#'
-    case '|'                                                           => '|'
+    case '.' if acre.adj(state)('|') >= 3 => '|'
+    case '.' => '.'
+    case '|' if acre.adj(state)('#') >= 3 => '#'
+    case '|' => '|'
     case '#' if acre.adj(state)('#') >= 1 && acre.adj(state)('|') >= 1 => '#'
-    case '#'                                                           => '.'
+    case '#' => '.'
   }
 
   def tick(state: State): State = state.par.map { case (acre, contents) => acre -> change(state)(acre, contents) }.seq
 
   def valueMap(state: State) = state.values.groupBy(identity).mapValues(_.size).withDefaultValue(0)
+  def valueOf(state: State) = valueMap(state)('|') * valueMap(state)('#')
 
-  val finalState =
-    Iterator
-      .from(1)
-      .take(10)
-      .foldLeft(initial)((now, _) => tick(now))
+  val states = Stream.from(1).scanLeft(initial)((now, _) => tick(now))
+  println("Part 1: " + valueOf(states(10)))
 
-  val partOne = valueMap(finalState)('|') * valueMap(finalState)('#')
-  println("Part 1: " + partOne)
+  val Some((again, second)) = firstDuplicateWithIndex(states)
+
+  // tiny bit of rework, could have been done in `firstDuplicateWithIndex`, but everything is memoized so it's fast
+  val Some(first) = states.zipWithIndex.collectFirst { case (orig, fst) if orig == again => fst }
+
+  val rewind = first + (1000000000 - first) % (second - first)
+
+  println("Part 2: " + valueOf(states(rewind)))
+
+  def firstDuplicateWithIndex[T](xs: Stream[T]): Option[(T, Int)] =
+    xs.scanLeft(Set.empty[T])(_ + _).zip(xs.zipWithIndex).collectFirst { case (s, (x, i)) if s contains x => x -> i }
 
 }
