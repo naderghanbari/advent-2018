c2a15b516b7301bf82373962ef1d6e4b44b4aad8
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2018-12-22 22:47:56 -0500

    Day 22 part 1

--- src/main/scala/aoc/Day22.scala
+++ src/main/scala/aoc/Day22.scala
@@ -2,7 +2,22 @@ package aoc
 
 object Day22 extends App {
 
+  val Depth = 7305
+  val Target = Region(13, 734)
 
+  case class Region(x: Int, y: Int)
 
+  def levelOf(index: Int) = (index + Depth) % 20183
+  def riskOf(level: Int) = level % 3
+
+  val zone = 0 to Target.x flatMap (x => 0 to Target.y map (x -> _)) map Region.tupled
+
+  val levels = (zone foldLeft Map.empty[Region, Int]) {
+    case (map, Target) => map + (Target -> levelOf(0))
+    case (map, r@Region(x, 0)) => map + (r -> levelOf(x * 16807))
+    case (map, r@Region(0, y)) => map + (r -> levelOf(y * 48271))
+    case (map, r@Region(x, y)) => map + (r -> levelOf(map(Region(x - 1, y)) * map(Region(x, y - 1))))
+  }
+
+  println(s"Part 1: ${levels.values.map(riskOf).sum}")
 }
-
